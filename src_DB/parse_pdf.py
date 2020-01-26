f2d0767e59641093c2624de61f5a782e045ddb5e
Marta Markowicz <marko090@umn.edu>
2020-01-26 01:18:17 -0600

    mod(parse_pdf.py): add comments

--- src_DB/parse_pdf.py
+++ src_DB/parse_pdf.py
@@ -10,26 +10,33 @@ all_files = []
 
 #loops through the list and prints metadata for each book
 for path in filepaths:
+    # print filepath
     print(path)
+
+    # make sure it's a pdf
     extension = os.path.splitext(path)[1]
     if(extension == ".pdf"):
         with open(path, 'rb') as fp:
+            # use pypdf2 to read metadata
             pdf = PdfFileReader(fp)
             info = pdf.getDocumentInfo()
-
             print("\tMETADATA:")
             for k, v in info.items():
                 print("\t\t", k, ": ", v)
 
+            # make sure first page can be text processed
+            # by checking if it has a font
             page_data = pdf.getPage(0)
             if '/Font' in page_data['/Resources']:
+                # process title using pdftitle by examining first page
                 title = pdftitle.get_title_from_io(fp)
                 if title is not None:
                     print("\tTITLE: " + title)
                 else:
                     print("\tTITLE: (none extracted)")
+
             else:
-                print("\t\tTitle: Cannot parse text on first page")
+                print("\tTitle: Cannot parse text on first page")
 
     else:
         print("\tNOT A PDF")
