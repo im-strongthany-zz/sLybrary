e344ccc136ee1f4c4fb55420a928425ff71731bf
Marta Markowicz <marko090@umn.edu>
2020-01-25 23:42:13 -0600

    mod(parse_pdf.py): switch to pdfpy2, parse title

--- src_DB/parse_pdf.py
+++ src_DB/parse_pdf.py
@@ -1,6 +1,7 @@
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
+
+from PyPDF2 import PdfFileReader
 import os
+import pdftitle
 
 #adds all the books to a list
 folderpath = r"./Books"
@@ -9,8 +10,26 @@ all_files = []
 
 #loops through the list and prints metadata for each book
 for path in filepaths:
-    with open(path, 'rb') as fp:
-        parser = PDFParser(fp)
-        doc = PDFDocument(parser)
-        print(doc.info)
+    print(path)
+    extension = os.path.splitext(path)[1]
+    if(extension == ".pdf"):
+        with open(path, 'rb') as fp:
+            pdf = PdfFileReader(fp)
+            info = pdf.getDocumentInfo()
 
+            print("\tMETADATA:")
+            for k, v in info.items():
+                print("\t\t", k, ": ", v)
+
+            page_data = pdf.getPage(0)
+            if '/Font' in page_data['/Resources']:
+                title = pdftitle.get_title_from_io(fp)
+                if title is not None:
+                    print("\tTITLE: " + title)
+                else:
+                    print("\tTITLE: (none extracted)")
+            else:
+                print("\t\tTitle: Cannot parse text on first page")
+
+    else:
+        print("\tNOT A PDF")
