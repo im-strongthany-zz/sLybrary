27f8b61dd7902f322c5401b58533d13cb9c942b6
Marta Markowicz <marko090@umn.edu>
2020-01-26 11:25:34 -0600

    mod(init): change to 0 ip

--- __init__.py
+++ __init__.py
@@ -63,19 +63,6 @@ def search_results(search):
     else:
         return render_template('results.html', results=results)
 
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-        return home()
-
-        if __name__ == "__main__":
-            app.secret_key = os.urandom(12)
-            app.run(debug=True,host='0.0.0.0', port=4000)
-
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
