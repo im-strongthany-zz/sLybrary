1d89df7a28c4bc96482d0038594eb61178a3886c
Matthew Kukla <macieleusz@gmail.com>
2020-01-25 22:18:35 -0600

    Update parse_pdf.py

--- src_DB/parse_pdf.py
+++ src_DB/parse_pdf.py
@@ -2,10 +2,12 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 import os
 
+#adds all the books to a list
 folderpath = r"./Books"
 filepaths = [os.path.join(folderpath,name) for name in os.listdir(folderpath)]
 all_files = []
 
+#loops through the list and prints metadata for each book
 for path in filepaths:
     with open(path, 'rb') as fp:
         parser = PDFParser(fp)
