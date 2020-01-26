dc0b76896fe5ad6d8b9616f438d5786e6a5e4dfa
Marta Markowicz <marko090@umn.edu>
2020-01-25 22:33:13 -0600

    mod(parse_pdf.py): add pdftitle extraction

--- src_DB/parse_pdf.py
+++ src_DB/parse_pdf.py
@@ -1,8 +1,15 @@
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
+import pdftitle
+# from pdfminer.pdftype import PDFObjRef, resolver1
 
-fp = open("/home/marta/sLybrary/src_DB/Books/[Brian_W._Kernighan,_Dennis_M._Ritchie]_The__C_Pro(BookZZ.org).pdf")
+fp = open("./Books/[Michael_Dawson]_Beginning_C++_through_game_progra(BookZZ.org).pdf", 'rb')
 parser = PDFParser(fp)
 doc = PDFDocument(parser)
 
 print(doc.info)
+
+title = pdftitle.get_title_from_io(fp)
+print(title)
+# print(type(doc.info))
+# print(doc.info[0]['Title'])
