0f887db6ad53480ffa9545ebccf2d940a8e8a141
macieleusz@gmail.com <kukla013@umn.edu>
2020-01-26 07:36:21 -0600

    Added ability to upload files

--- __init__.py
+++ __init__.py
@@ -1,8 +1,35 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from form import BookSearchForm
 import os
+import urllib.request
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__, template_folder='src_DB/templates')
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['POST'])
+def upload_file():
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File successfully uploaded')
+			return redirect('/')
+		else:
+			flash('Allowed file types are pdf')
+			return redirect(request.url)
 
 @app.route('/', methods=['GET','POST'])
 def index():
